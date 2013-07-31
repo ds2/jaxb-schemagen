00c67ccf1b06c7caaf202c8a6200563bd655b2f3
dstrauss <dstrauss@ds-2.de>
2013-07-31 11:46:43 +0200

    SCHEMAGEN-0 it compiles

---
+++ schemagen-maven-plugin/src/main/java/ds2/oss/maven/plugins/schemagen/plugin/impl/SchemaGenMojo.java
@@ -0,0 +1,17 @@
+package ds2.oss.maven.plugins.schemagen.plugin.impl;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Mojo;
+
+/**
+ * Created by dstrauss on 31.07.13.
+ */
+@Mojo(name="schemagen")
+public class SchemaGenMojo extends AbstractMojo{
+  @Override
+  public void execute() throws MojoExecutionException, MojoFailureException {
+
+  }
+}
