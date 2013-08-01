e97aa9181319a409085b4cb8e4e60171c348871c
dstrauss <dstrauss@ds-2.de>
2013-08-01 08:25:58 +0200

    SCHEMAGEN-0 naming, setup phases and runtime deps

--- schemagen-maven-plugin/src/main/java/ds2/oss/maven/plugins/schemagen/plugin/impl/SchemaGenMojo.java
+++ schemagen-maven-plugin/src/main/java/ds2/oss/maven/plugins/schemagen/plugin/impl/SchemaGenMojo.java
@@ -3,15 +3,19 @@ package ds2.oss.maven.plugins.schemagen.plugin.impl;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.ResolutionScope;
 
 /**
- * Created by dstrauss on 31.07.13.
+ * The schemagen mojo.
+ * @author dstrauss
+ * @version 0.1
  */
-@Mojo(name="schemagen")
+@Mojo(name="schemagen",defaultPhase = LifecyclePhase.GENERATE_RESOURCES,requiresDependencyResolution = ResolutionScope.COMPILE_PLUS_RUNTIME,threadSafe = true)
 public class SchemaGenMojo extends AbstractMojo{
   @Override
   public void execute() throws MojoExecutionException, MojoFailureException {
-
+    getLog().info("Creating world...");
   }
 }
